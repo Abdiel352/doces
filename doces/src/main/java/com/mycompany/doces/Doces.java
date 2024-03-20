f499a3cd30501176a85df208ea0371d188e89e18
Abdiel352 <120688631+Abdiel352@users.noreply.github.com>
2024-03-20 13:28:18 -0400

    Add files via upload

---
+++ doces/src/main/java/com/mycompany/doces/Doces.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.doces;
+
+/**
+ *
+ * @author Aluno
+ */
+import javax.swing.*;
+public class Doces {
+
+    public static void main(String[] args) {
+        
+        pessoa frame = new pessoa();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLayout(null); 
+        frame.setVisible(true);
+        
+    }
+}
